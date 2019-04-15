369c2fe13da2168397928bbf812896fa898bea62
Eric Escalante <eric.m.escalante@gmail.com>
2019-04-14 20:12:16 -0500

    Project continued

--- Clustering/acquire_zillow.py
+++ Clustering/acquire_zillow.py
@@ -32,11 +32,13 @@ def zillow_properties_2017():
                         LEFT JOIN typeconstructiontype ttype USING (typeconstructiontypeid)', 
     get_connection('zillow'))
 
+### Function is used to join the two created data frames together and returns a CSV of the merged dataframes
 def get_zillow_csv():
     df_2016 = zillow_properties_2016()
     df_2017 = zillow_properties_2017()
     zillow = df_2016.append(df_2017, ignore_index=True)
     return zillow.to_csv(r'../Clustering/zillow_properties.csv', index=False)
 
+### Function is to read the actual CSV so that we do not continue to build duplicated csv files
 def read_zillow_csv():
     return pd.read_csv("./zillow_properties.csv", low_memory=False, index_col ='parcelid')
--- Clustering/prepare_zillow.py
+++ Clustering/prepare_zillow.py
@@ -3,39 +3,49 @@ import acquire_zillow
 import pandas as pd
 import numpy as np
 
+
+
+### Dropping multiple columns that we will not use in our model, descriptions of why are in the Jupyter file
+def drop_columns(df):
+    return df.drop(columns=['heatingorsystemdesc', 'censustractandblock',
+                     'regionidcity', 'finishedsquarefeet12',
+                     'rawcensustractandblock', 'propertyzoningdesc',
+                     'transactiondate',
+                     'calculatedbathnbr','fullbathcnt',
+                     'propertycountylandusecode', 'taxamount'])
+
+### Our focus will be on the rows with the unit count of less than 2; we also want to look at the units that actually have a bedroom count and a calculated square feet of over 500 square feet
+def drop_units(df):
+    unit_one = df[(df['unitcnt'] >= 2) & (df['bedroomcnt'] >= 2) & (df['calculatedfinishedsquarefeet'] >= 500)].index 
+    df = df.drop(unit_one, inplace=True)
+    return df
+
+### Run the first function that returns missing value totals by column: Does the attribute have enough information (i.e. enough non-null values) to be useful? Choose your cutoff and remove columns where there is not enough information available. Document your cutoff and your reasoning.
+def replace_null_data(df, column_name, numerical = True):
+    if numerical == True:
+        df[column_name] = df[column_name].fillna(df[column_name].mean())
+    else:
+        df[column_name] = df[column_name].fillna(df[column_name].mode()[0])
+    
+    return df
+
+### Write or use a previously written function to return the total missing values and the percent missing values by column.
+def null_percent_col(df):
+    return df.isnull().sum() / df.shape[0] * 100.00
+
+### Write or use a previously written function to return the total missing values and the percent missing values by row.
+def null_percent_row(df):
+    return df.isnull().sum() / df.shape[1] * 100.00
+
+### Write a function that will take a dataframe and list of column names as input and return the dataframe with the null values in those columns replace by 0.
+def replace_with_zero(df, column_name):
+    df[column_name] = df[column_name].fillna(0)
+    return df
+
+### We will prep a few functions in prepare.py; however we will use most of the functions in the Jupyter file
+def prep_zillow(df):
+    df = df.pipe(drop_columns).pipe(drop_units)
+    return df
+
 df = acquire_zillow.read_zillow_csv()
-
-def remove_columns(df, cols_to_remove):
-    df = df.drop(columns=cols_to_remove)
-    return df
-
-def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
-    threshold = int(round(prop_required_column*len(df.index),0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
-    threshold = int(round(prop_required_row*len(df.columns),0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
-    return df
-
-def only_one_unit():
-    unit_one = df[(df['unitcnt'] == 1) & (df['bedroomcnt'] >= 1)].index    
-    return df.drop(unit_one, inplace=True)
-
-def remove_proerty_land():
-    df[df.propertylandusedesc != 'Duplex (2 Units, Any Combination)']
-    df[df.propertylandusedesc != 'Quadruplex (4 Units, Any Combination)']
-    df[df.propertylandusedesc != 'Triplex (3 Units, Any Combination)']
-    return df
-
-def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75):
-    df = remove_columns(df, cols_to_remove)
-    df = handle_missing_values(df, prop_required_column, prop_required_row)
-    df = df.dropna(axis=0, subset=['longitude'])
-    return df
-
-data_prep(df, cols_to_remove=['airconditioningtypeid', 'architecturalstyletypeid', 'buildingclasstypeid','buildingqualitytypeid', 'propertylandusetypeid', 'typeconstructiontypeid', 'storytypeid', 'heatingorsystemtypeid'])
-
-'''
-### use this later in exploration.py
-bycounty = df.groupby('regionidcounty')
-print(bycounty['logerror'].describe())
-'''
+print(df.shape)s
