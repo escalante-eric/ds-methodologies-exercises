32c091a843c257efd7e99c586a27653e9b353019
Eric Escalante <eric.m.escalante@gmail.com>
2019-04-17 14:03:44 -0500

    finished access log

--- time_series/access_log.py
+++ time_series/access_log.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('http://python.zach.lol/access.log',
-              sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+df = pd.read_csv('http://python.zach.lol/access.log',          
               engine='python',
-              usecols=[0, 3, 4, 5, 6, 7, 8],
+              header=None,
+              index_col=False,
               names=['ip', 'timestamp', 'request_method', 'status', 'size', 'unknown', 'request_agent'],
               na_values='-',
-              header=None,
-              index_col=False)
+              sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+              usecols=[0, 3, 4, 5, 6, 7, 8])
 
 print(df.tail(5))
