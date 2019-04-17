f9332f05589c5abf270ccea7a5f5e98c346b92c5
Eric Escalante <eric.m.escalante@gmail.com>
2019-04-17 13:33:23 -0500

    updated access log

--- time_series/access_log.py
+++ time_series/access_log.py
@@ -4,7 +4,7 @@ df = pd.read_csv('http://python.zach.lol/access.log',
               sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
               engine='python',
               usecols=[0, 3, 4, 5, 6, 7, 8],
-              names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+              names=['ip', 'time', 'request', 'status', 'size', 'referer', 'request_agent'],
               na_values='-',
               header=None)
 
