38a0472af5451b86a32ec93a855de4a71dcdaac6
Eric Escalante <eric.m.escalante@gmail.com>
2019-04-17 15:49:00 -0500

    done with access log

--- time_series/access_log.py
+++ time_series/access_log.py
@@ -1,12 +1,22 @@
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv('http://python.zach.lol/access.log',          
               engine='python',
               header=None,
               index_col=False,
-              names=['ip', 'timestamp', 'request_method', 'status', 'size', 'unknown', 'request_agent'],
-              na_values='-',
+              names=['ip', 'timestamp', 'request_method', 'status', 'size', 'request_agent'],
               sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-              usecols=[0, 3, 4, 5, 6, 7, 8])
+              na_values='-',
+              usecols=[0, 3, 4, 5, 6, 8])
 
-print(df.tail(5))
+df.timestamp = df.timestamp.str.replace('[', '')
+df.timestamp = df.timestamp.str.replace(']', '')
+df.timestamp= pd.to_datetime(df.timestamp.str.replace(':', ' ', 1)) 
+
+df.request_method = df.request_method.str.replace('"', '')
+
+df = df.set_index('timestamp')
+df = df.tz_localize('utc').tz_convert('America/Chicago')
+
+print(df.head())
