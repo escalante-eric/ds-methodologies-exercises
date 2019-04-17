0cbeb20faee7739a688638a31a62c990cde071a9
Eric Escalante <eric.m.escalante@gmail.com>
2019-04-17 13:32:17 -0500

    added access log exercise

---
+++ time_series/access_log.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('http://python.zach.lol/access.log',
+              sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+              engine='python',
+              usecols=[0, 3, 4, 5, 6, 7, 8],
+              names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+              na_values='-',
+              header=None)
+
+print(df.sample(30))
